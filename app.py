1aa50f3065ed164ed556de4059471715361e179f
Roberta Drayer <robertadrayer@gmail.com>
2023-06-08 13:03:43 -0700

    completed steps 0 - 7 of exercise, added logic in question/answer routes, flash message

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
 from flask import Flask, request, render_template, redirect, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension 
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chickens1'
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
+responses = []
 
-def test():
-    '''
-    Create a new virtual environment and activate it.
+@app.route('/')
+def home():
+    '''Home Page'''
+    #current_question_no = len(responses)
+    #questions = satisfaction_survey.questions
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('home.html', title = title, instructions = instructions, responses = responses)
 
-    Install Flask and the Flask Debug Toolbar.
+@app.route('/questions/<int:question_no>')
+def display_questions(question_no):
+    '''Display question'''
+    current_question_no = len(responses)
 
-    Make your project a Git repository, 
-    and add ***venv/*** and ***__pycache__*** to a
-    new ***.gitignore*** file for your repository.
+    if current_question_no < question_no or current_question_no > question_no:
+        flash("You're trying to access an invalid question")
+        return redirect ('/questions/' + str(current_question_no))
     
-    '''
+    question = satisfaction_survey.questions[question_no].question
+
+    return render_template('questions.html', question = question, current_question_no = current_question_no, question_no = question_no)
+
+@app.route('/answer', methods=["POST"])
+def track_answer():
+    ans = request.form['yes_no']
+    responses.append(ans)
+    if len(responses) == 1:
+        return redirect ('/questions/1')
+    elif len(responses) == 2:
+        return redirect ('/questions/2')    
+    elif len(responses) == 3:
+        return redirect ('/questions/3')
+    else:
+        return render_template ('thank_you.html', responses = responses)
+    
